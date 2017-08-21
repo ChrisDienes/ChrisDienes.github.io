e9d7268426d72a68d8d01edbc779f339c52bd674
ChrisDienes <26510690+ChrisDienes@users.noreply.github.com>
2017-08-21 14:32:11 -0600

    Update myCustom.js

--- myCustom.js
+++ myCustom.js
@@ -1,6 +1,8 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    // -----------------------------------------------------------------------
     $.each($('#navbar').find('li'), function() {
-        $(this).toggleClass('active',
-            '/' + $(this).find('a').attr('href') == window.location.pathname);
-    });
+        $(this).toggleClass('active', 
+            window.location.pathname.indexOf($(this).find('a').attr('href')) > -1);
+    }); 
+    // -----------------------------------------------------------------------
 });
